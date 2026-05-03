2306d3369a18d2498700879073d02ad82d60eac8
KollaNagaManasa <2024tm93519@wilp.bits-pilani.ac.in>
2026-05-03 22:23:30 +0530

    Update NotificationListener.java

--- notification-service/src/main/java/edu/bits/notification/NotificationListener.java
+++ notification-service/src/main/java/edu/bits/notification/NotificationListener.java
@@ -3,11 +3,23 @@ package edu.bits.notification;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class NotificationListener {
 
     @RabbitListener(queues = "txn.notification.queue")
-    public void handleMessage(String message) {
-        System.out.println("🔥 Received message: " + message);
+    public void handleMessage(Map<String, Object> message) {
+
+        System.out.println(" Notification received!");
+
+        System.out.println("Event Type: " + message.get("eventType"));
+        System.out.println("From Account: " + message.get("fromAccount"));
+        System.out.println("To Account: " + message.get("toAccount"));
+        System.out.println("Amount: " + message.get("amount"));
+        System.out.println("Reference: " + message.get("reference"));
+        System.out.println("Correlation ID: " + message.get("correlationId"));
+
+        System.out.println("Full Payload: " + message);
     }
 }

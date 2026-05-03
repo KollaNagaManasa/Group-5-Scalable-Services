2878932f48aa9b88fcc082f9653ae3abdbde69bc
KollaNagaManasa <2024tm93519@wilp.bits-pilani.ac.in>
2026-05-03 19:47:09 +0530

    Update NotificationServiceApplication.java

--- notification-service/src/main/java/edu/bits/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/edu/bits/notification/NotificationServiceApplication.java
@@ -1,17 +1,13 @@
+package edu.bits.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 
 @EnableRabbit
 @SpringBootApplication
 public class NotificationServiceApplication {
-}
 
-package edu.bits.notification;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
     }

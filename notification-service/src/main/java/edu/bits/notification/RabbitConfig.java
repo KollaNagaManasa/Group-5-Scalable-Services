6f8fda4cf6adf81ebccbdb6e10160cd0e1019cb4
KollaNagaManasa <2024tm93519@wilp.bits-pilani.ac.in>
2026-05-03 22:46:08 +0530

    Update RabbitConfig.java

--- notification-service/src/main/java/edu/bits/notification/RabbitConfig.java
+++ notification-service/src/main/java/edu/bits/notification/RabbitConfig.java
@@ -1,31 +1,26 @@
 package edu.bits.notification;
 
-import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.*;
 
 @Configuration
 public class RabbitConfig {
 
-    public static final String EXCHANGE = "txn.exchange";
-    public static final String QUEUE = "txn.notification.queue";
-    public static final String ROUTING_KEY = "txn.notification";
-
     @Bean
-    public DirectExchange exchange() {
-        return new DirectExchange(EXCHANGE);
+    public Queue queue() {
+        return new Queue("txn.notification.queue", true);
     }
 
     @Bean
-    public Queue queue() {
-        return new Queue(QUEUE, true);
+    public DirectExchange exchange() {
+        return new DirectExchange("txn.exchange");
     }
 
     @Bean
     public Binding binding(Queue queue, DirectExchange exchange) {
-        return BindingBuilder
-                .bind(queue)
+        return BindingBuilder.bind(queue)
                 .to(exchange)
-                .with(ROUTING_KEY);
+                .with("txn.created");
     }
 }
